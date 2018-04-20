4ad47ee6ca59c570da93f84e78e0f6325a43486d
Atsushi Hara <toohsk@gmail.com>
2018-04-20 10:04:03 +0900

    SocketIO Flask app initial code.
    Add comment.

--- app.py
+++ app.py
@@ -1,15 +1,12 @@
-from threading import Lock
-from flask import Flask, render_template, session, request
+from flask import Flask
+from flask_socketio import SocketIO
 
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
-
-
-@app.route('/')
-def index():
-    return render_template('index.html')
-
+socketio = SocketIO(app)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080)
+    # 通常のFlaskの場合，app.run()とするが，
+    # SocketIOを利用する場合，socketio.run()とする
+    socketio.run(app)
